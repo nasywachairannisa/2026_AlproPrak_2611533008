592ac4b2b50715a85642145cf560f4feafd027f1
Nasywa Chairannisa <nasywachairannisa2@gmail.com>
2026-09-03 12:30:05 +0700

    Praktikum pekan 1

---
+++ Pekan 1/program1_2611533008.py
@@ -0,0 +1,14 @@
+# program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini " + "Dibuat oleh " 
++ "nasywa_2611533008.")
+
