bf6b10125b44527380dd721b68e3324de4d841ec
nasywachairannisa <nasywachairannisa2@gmail.com>
2026-09-10 19:04:50 +0700

    Add files via upload

---
+++ Pekan 1/tugas1_2611533008.py
@@ -0,0 +1,10 @@
+# Program Suasana Hari Ini
+
+print("=== SUASANA HARI INI ===")
+print()
+print("Cuaca     : Mendung")
+print("Suasana   : Tenang")
+print("Mood      : Santai")
+print("Aktivitas : Mendengarkan musik")
+print()
+print("Hari yang tenang untuk beristirahat.")
